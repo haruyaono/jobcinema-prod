4ed5f9c09073be80f2d134d4c03275e9dad6e8da
haruya ono <haru@haruyaono.tokyo>
2020-05-09 00:34:18 +0900

    Create setBaseImageUrlAndSetEnvName function in common.js

---
+++ public/js/common.js
@@ -0,0 +1,28 @@
+(function( $ ) {
+'use strict';
+
+    $.fn.setBaseImageUrlAndSetEnvName = function(){
+
+        var array = [];
+
+        if($('#env_name').length) {
+            var env_name = $('#env_name').attr('value');
+            if(env_name == 'production') {
+                var base_image_url = 'https://s3.job-cinema.com';
+            } else {
+                var base_image_url = '';
+            }
+        } else {
+            var base_image_url = '';
+            var env_name = '';
+        }
+
+        array = {
+            base_image_url : base_image_url,
+            env_name : env_name
+        }
+
+        return array;
+    }
+    
+})( jQuery );
