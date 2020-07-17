d88097cb981d1e1bd2840edcaea73de3318a0339
haruya ono <haru@haruyaono.tokyo>
2020-07-17 11:57:07 +0900

    Fixed design collapse on smartphone.

--- resources/views/jobs/history.blade.php
+++ resources/views/jobs/history.blade.php
@@ -4,85 +4,85 @@
 @section('description', '釧路の職場を上映する求人サイト')
 
 @section('header')
-  @component('components.header')
-  @endcomponent
+@component('components.header')
+@endcomponent
 @endsection
 
 @section('contents')
 <!-- パンくず -->
 <div id="breadcrumb" class="bread only-pc">
-<ol>
-  <li>
-    <a href="/">
-      <i class="fa fa-home"></i><span>釧路の求人情報TOP</span>
-    </a>
-  </li>
-  <li>
-    <a>
-    最近見た求人
-    </a>
-  </li>
-</ol>
+  <ol>
+    <li>
+      <a href="/">
+        <i class="fa fa-home"></i><span>釧路の求人情報TOP</span>
+      </a>
+    </li>
+    <li>
+      <a>
+        最近見た求人
+      </a>
+    </li>
+  </ol>
 </div>
 
 <div class="main-wrap">
-<section class="main-section job-entry">
-<div class="inner">
-<div class="pad">
-    <h1 class="txt-h2">最近見た求人</h1>
-      <div class="job-list">
-      @if ($jobs !== [])
+  <section class="main-section job-entry">
+    <div class="inner">
+      <div class="pad">
+        <h1 class="txt-h2">最近見た求人</h1>
+        <div class="job-list">
+          @if ($jobs !== [])
           @foreach ($jobs as $job)
           <div class="job-item">
-          <a href="{{ route('jobs.show', [$job->id])}}" class="job-item-link">
-            <div class="job-item-heading only-pc">
-              <!-- カテゴリ -->
-              <span class="cat-item org">{{$job->categories()->wherePivot('slug', 'type')->first() !== null ? $job->categories()->wherePivot('slug', 'type')->first()->name : ''}}</span>
-              <span class="cat-item red">{{$job->categories()->wherePivot('slug', 'status')->first() !== null ? $job->categories()->wherePivot('slug', 'status')->first()->name : ''}}</span>
-            </div>
-            <div class="title-img-wrap">
-            <div class="job-left only-sp">
-              @if(($job->job_img) ==! null)
-                <img src="@if(config('app.env') == 'production'){{config('app.s3_url')}}{{$job->job_img}}@else{{$job->job_img}}@endif" alt=""/>
-                @endif
+            <a href="{{ route('jobs.show', [$job->id])}}" class="job-item-link">
+              <div class="job-item-heading only-pc">
+                <!-- カテゴリ -->
+                <span class="cat-item org">{{$job->categories()->wherePivot('slug', 'type')->first() !== null ? $job->categories()->wherePivot('slug', 'type')->first()->name : ''}}</span>
+                <span class="cat-item red">{{$job->categories()->wherePivot('slug', 'status')->first() !== null ? $job->categories()->wherePivot('slug', 'status')->first()->name : ''}}</span>
               </div>
-              <h2 class="txt-h2">
-                {{$job->job_title}}
-                <p class="company_name_item">{{$job->company->cname}}</p>
-              </h2>
-              
-            </div>
+              <div class="jobCassette__header">
+                <div class="job-left only-sp">
+                  @if(($job->job_img) ==! null)
+                  <img src="@if(config('app.env') == 'production'){{config('app.s3_url')}}{{$job->job_img}}@else{{$job->job_img}}@endif" alt="" />
+                  @endif
+                </div>
+                <div class="jobCassette__title">
+                  <p class="jobCassette__jobTypeTxt"> {{$job->job_title}}</p>
+                  <h2 class="company_name_item">{{$job->company->cname}}</h2>
+                </div>
 
-            <div class="d-flex">
-              <div class="job-left only-pc">
-                @if(($job->job_img) ==! null)
-                <img src="@if(config('app.env') == 'production'){{config('app.s3_url')}}{{$job->job_img}}@else{{$job->job_img}}@endif" style="width:100%;" alt=""/>
-                @else
-                <img src="{{ asset('uploads/images/no-image.gif')}}" style="width:100%;" alt="No image">
-                @endif
               </div>
-              <div class="job-right">
-                <table class="job-table">
-                  <tr>
-                    <th><span class="money"><span>給与</span></span></th>
-                    <td>{{ str_limit($job->job_hourly_salary, $limit = 40, $end = '...')}}</td>
-                  </tr>
-                  <tr>
-                    <th><span class="place"><span>勤務先</span></span></th>
-                    <td>{{ str_limit($job->job_office, $limit = 40, $end = '...')}}</td>
-                  </tr>
-                  <tr>
-                    <th><span class="work"><span>仕事内容</span></span></th>
-                    <td>{{ str_limit($job->job_desc, $limit = 80, $end = '...')}}</td>
-                  </tr>
-                  <tr>
-                    <th><span class="time"><span>勤務時間</span></span></th>
-                    <td>{{ str_limit($job->job_time, $limit = 40, $end = '...')}}</td>
-                  </tr>
-                </table>
+
+              <div class="d-flex">
+                <div class="job-left only-pc">
+                  @if(($job->job_img) ==! null)
+                  <img src="@if(config('app.env') == 'production'){{config('app.s3_url')}}{{$job->job_img}}@else{{$job->job_img}}@endif" style="width:100%;" alt="" />
+                  @else
+                  <img src="{{ asset('uploads/images/no-image.gif')}}" style="width:100%;" alt="No image">
+                  @endif
+                </div>
+                <div class="job-right">
+                  <table class="job-table">
+                    <tr>
+                      <th><span class="money"><span>給与</span></span></th>
+                      <td>{{ str_limit($job->job_hourly_salary, $limit = 40, $end = '...')}}</td>
+                    </tr>
+                    <tr>
+                      <th><span class="place"><span>勤務先</span></span></th>
+                      <td>{{ str_limit($job->job_office, $limit = 40, $end = '...')}}</td>
+                    </tr>
+                    <tr>
+                      <th><span class="work"><span>仕事内容</span></span></th>
+                      <td>{{ str_limit($job->job_desc, $limit = 80, $end = '...')}}</td>
+                    </tr>
+                    <tr>
+                      <th><span class="time"><span>勤務時間</span></span></th>
+                      <td>{{ str_limit($job->job_time, $limit = 40, $end = '...')}}</td>
+                    </tr>
+                  </table>
+                </div>
               </div>
-            </div>
-            <!-- <div class="jobitem-footer" @if(!$job->pubend_at) style="display:block; text-align:right;" @endif>
+              <!-- <div class="jobitem-footer" @if(!$job->pubend_at) style="display:block; text-align:right;" @endif>
               @if($job->pubend_at)
               <span>{{ $job->pubend_at->format('Y年m月d日') }}に掲載終了</span>
               @endif
@@ -90,25 +90,25 @@
                 <a href="{{ route('jobs.show', [$job->id, $job->slug])}}" class="btn detail-btn"><span>詳しく見る</span></a>
               </object>
             </div> -->
-          </a>
-        </div> <!-- newjob-item -->
+            </a>
+          </div> <!-- newjob-item -->
           <!-- △ ループ終了 △ -->
           @endforeach
-      @else
+          @else
           <p class="no-jobitem-txt">閲覧した求人は現在ありません。</p>
-      @endif
-              
+          @endif
+
+        </div>
+      </div><!-- pad -->
+      <div class="paginate text-center">
+        {{ $jobs !== [] ? $jobs->links() : ''}}
       </div>
-    </div><!-- pad -->
-    <div class="paginate text-center">
-      {{  $jobs !== [] ? $jobs->links() : ''}}
-    </div>
-  </div> <!-- inner -->
-</section> <!-- newjob-entry -->
+    </div> <!-- inner -->
+  </section> <!-- newjob-entry -->
 </div> <!-- main-wrap-->
 @endsection
 
 @section('footer')
-  @component('components.footer')
-  @endcomponent
+@component('components.footer')
+@endcomponent
 @endsection
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -4,8 +4,8 @@
 @section('description', '釧路の職場を上映する求人サイト')
 
 @section('header')
-  @component('components.header')
-  @endcomponent
+@component('components.header')
+@endcomponent
 @endsection
 
 @section('contents')
@@ -16,17 +16,17 @@
 
 <!-- ここからメインコンテンツ -->
 <div class="main-wrap">
-<!-- @if (Auth::guard()->check())
+	<!-- @if (Auth::guard()->check())
 <p>求職者ログイン中</p>
 @endif
 @if (Auth::guard('employer')->check())
 <p>企業ログイン中</p>
 @endif -->
 	<!-- 絞り込み・検索エリア -->
-<div class="only-pc">
-	<search-component></search-component>
-</div>
-  
+	<div class="only-pc">
+		<search-component></search-component>
+	</div>
+
 	<section class="main-section">
 		<div class="inner">
 			<div class="pad">
@@ -35,14 +35,14 @@
 					<div class="newjob-list-border">
 						<div class="newjob-list">
 							<!-- ▽ ループ開始 ▽ -->
-						@forelse ($topNewJobs as $job)
+							@forelse ($topNewJobs as $job)
 							<div class="newjob-item">
 								<a href="{{ route('jobs.show', [$job->id])}}" class="newjob-item-link">
-									<p class="img-wrap">
+									<p class="joblist_jobCassette__image_wrap">
 										@if(($job->job_img) !== null)
-											<img src="@if(config('app.env') == 'production'){{config('app.s3_url')}}{{$job->job_img}}@else{{asset($job->job_img)}}@endif" style="width:100%;" alt=""/>
+										<img src="@if(config('app.env') == 'production'){{config('app.s3_url')}}{{$job->job_img}}@else{{asset($job->job_img)}}@endif" style="width:100%;" alt="" />
 										@else
-											<img src="{{ asset('uploads/images/no-image.gif')}}" style="width:100%;" alt="No image">
+										<img src="{{ asset('uploads/images/no-image.gif')}}" style="width:100%;" alt="No image">
 										@endif
 									</p>
 									<div class="txt-wrap">
@@ -55,45 +55,45 @@
 											<p><span class="place"><span>{{ str_limit($job->job_office_address, $limit = 40, $end = '...')}}</p>
 										</div>
 									</div>
-										
+
 								</a>
 							</div> <!-- newjob-item -->
 							<!-- △ ループ終了 △ -->
-						@empty
-						<p>新着案件がありません。</p>
-						@endforelse
+							@empty
+							<p>新着案件がありません。</p>
+							@endforelse
 						</div> <!-- newjob-list -->
 						<p class="more-link">
-						<a href="{{ route('alljobs') }}">求人一覧を見る</a>
-					</p>
+							<a href="{{ route('alljobs') }}">求人一覧を見る</a>
+						</p>
 					</div>
 				</div>
 
 				@include('partials.type_categories')
 				@include('partials.area_categories')
-				
+
 				@if(!Auth::check())
 				<div class="main-section-item top-subsection-item only-sp" sty>
 					<div class="top-sp-auth-btn-wrap">
-						<p><a class="btn btn-yellow w-100 mb-3"  href="/members/register">会員登録</a></p>
+						<p><a class="btn btn-yellow w-100 mb-3" href="/members/register">会員登録</a></p>
 						<p><a class="btn btn-outline-secondary w-100" href="/members/login">ログイン</a></p>
 					</div>
-					
+
 				</div>
 				@endif
 
 				<recent-component></recent-component>
-				
+
 				<div class="main-section-item top-subsection-item">
 					<div class="top-subsection-item-inner">
 						<h2>アルバイト/正社員の求人情報JOBCiNEMA
 						</h2>
 						<div class="top-subsection-item-text">
-						釧路に特化した求人情報サイト「JOBCiNEMA」では、アルバイト採用者5,000円/正社員採用者1万円のお祝い金をプレゼントしております！<br>釧路のお仕事情報を地域、職種、時給などさまざまな方法で検索可能です。<br>職場の雰囲気をリアルに伝えるために多数の動画を掲載！
+							釧路に特化した求人情報サイト「JOBCiNEMA」では、アルバイト採用者5,000円/正社員採用者1万円のお祝い金をプレゼントしております！<br>釧路のお仕事情報を地域、職種、時給などさまざまな方法で検索可能です。<br>職場の雰囲気をリアルに伝えるために多数の動画を掲載！
 						</div>
 					</div>
 				</div>
-				
+
 			</div> <!-- pad -->
 		</div> <!-- inner -->
 	</section> <!-- newjob-entry -->
@@ -102,20 +102,23 @@
 @endsection
 
 @section('footer')
-  @component('components.footer')
-  @endcomponent
+@component('components.footer')
+@endcomponent
 @endsection
 
 
 @section('js')
 <script type="text/javascript" refer>
-    $(function(){
-        $(".wide-notice-overlay").show();
-        Cookies.get('btnFlg') == 'on'?$(".wide-notice-overlay").hide():$(".wide-notice-overlay").show();
-        $(".notice-close").click(function(){
-            $(".wide-notice-overlay").fadeOut();
-			Cookies.set('btnFlg', 'on', { expires: 30,path: '/' }); //cookieの保存
-			
+	$(function() {
+		$(".wide-notice-overlay").show();
+		Cookies.get('btnFlg') == 'on' ? $(".wide-notice-overlay").hide() : $(".wide-notice-overlay").show();
+		$(".notice-close").click(function() {
+			$(".wide-notice-overlay").fadeOut();
+			Cookies.set('btnFlg', 'on', {
+				expires: 30,
+				path: '/'
+			}); //cookieの保存
+
 		});
 	});
 </script>
