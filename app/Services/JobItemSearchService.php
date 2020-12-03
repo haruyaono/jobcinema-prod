09f3a2d198a45d1c8cb94317b858cb98ca90202c
haruya ono <haru@haruyaono.tokyo>
2020-12-03 23:31:15 +0900

    Fixed jobs searching function.
    Allowed sorting by job category and congratulatory amount.

--- app/Services/JobItemSearchService.php
+++ app/Services/JobItemSearchService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\JobItem;
+use DB;
 
 class JobItemSearchService
 {
@@ -18,7 +19,8 @@ class JobItemSearchService
     public function search(array $searchParam = [])
     {
         $query = JobItem::activeJobitem()->with([
-            'categories'
+            'categories',
+            'categories.congratsMoney'
         ]);
 
         $newsearchParam = $searchParam;
@@ -57,28 +59,42 @@ class JobItemSearchService
             });
         }
 
-        if (isset($searchParams['ks'])) {
-            $orderFlag = $searchParams['ks'];
+        if (!isset($searchParams['ks'])) return;
 
-            if (array_key_exists('slug', $orderFlag) && $orderFlag['slug'] != '') {
+        $orderFlag = $searchParams['ks'];
 
-                $query->join('job_item_category', 'job_items.id', '=', 'job_item_category.job_item_id');
+        if (array_key_exists('slug', $orderFlag) && $orderFlag['slug'] != '') {
 
-                if ($orderFlag['slug'] == 'salary') {
-                    $query->where('job_item_category.parent_slug', $orderFlag['parent']);
-                } else {
-                    $query->where('job_item_category.ancestor_slug', $orderFlag['slug']);
+            $query->join('job_item_category', 'job_items.id', '=', 'job_item_category.job_item_id')
+                ->select('job_item_category.category_id', 'job_item_category.parent_slug', 'job_item_category.ancestor_slug');
+
+            if ($orderFlag['slug'] == 'salary') {
+                $query->where('job_item_category.parent_slug', $orderFlag['parent']);
+            } else {
+                $query->where('job_item_category.ancestor_slug', $orderFlag['slug'] == 'oiwaikin' ? 'status' : $orderFlag['slug']);
+            }
+
+            $query->join('categories', 'job_item_category.category_id', '=', 'categories.id');
+
+            if ($orderFlag['slug'] == 'oiwaikin') {
+
+                $rewards = DB::table('congrats_monies')
+                    ->select('category_id', 'amount');
+
+                $query->joinSub($rewards, 'rewards', function ($join) {
+                    $join->on('categories.id', '=', 'rewards.category_id');
+                })->select('categories.id as category_id', 'rewards.amount');
+            }
+
+            $query->addSelect('job_items.*');
+
+            if (array_key_exists('order', $orderFlag) && $orderFlag['order'] != '') {
+                $query->orderBy('categories.sort', $orderFlag['order']);
+            } else {
+                if ($orderFlag['slug'] == 'oiwaikin') {
+                    $query->orderBy('rewards.amount', 'desc');
                 }
-
-                $query->join('categories', 'job_item_category.category_id', '=', 'categories.id')
-                    ->orderBy('categories.sort', 'desc')
-                    ->select('job_items.*');
-
-                if (array_key_exists('order', $orderFlag) && $orderFlag['order'] != '') {
-                    $query->orderBy('categories.sort', $orderFlag['order']);
-                } else {
-                    $query->orderBy('categories.sort', 'desc');
-                }
+                $query->orderBy('categories.sort', 'desc');
             }
         }
     }
