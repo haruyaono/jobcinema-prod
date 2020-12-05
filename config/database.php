7c4cc03f9cd035b3ed0c5df035ace5359b5f4afc
コノハ <46505449+konoha-okemaru@users.noreply.github.com>
2020-12-05 16:22:49 +0900

    Update database.php

--- config/database.php
+++ config/database.php
@@ -140,21 +140,20 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'predis'),
-
-        'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 0),
-            'read_write_timeout' => env('REDIS_READ_WRITE_TIMEOUT', 600),
+        'client' => 'predis',
+        'options' => [
+            'cluster' => 'redis',
         ],
-
-        'cache' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_CACHE_DB', 1),
+        'clusters' => [
+            'default' => [
+                [
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'password' => env('REDIS_PASSWORD', null),
+                    'port' => env('REDIS_PORT', 6379),
+                    'database' => 0,
+                    'read_write_timeout' => env('REDIS_READ_WRITE_TIMEOUT', 60),
+                ]
+            ],
         ],
 
     ],
