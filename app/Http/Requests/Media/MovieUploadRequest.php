a5afd5fd0210f270383de61f7a94973faa731d51
haruya ono <haru@haruyaono.tokyo>
2020-12-05 14:20:38 +0900

    Fixed validation of job vacancies media upload.

--- app/Http/Requests/Media/MovieUploadRequest.php
+++ app/Http/Requests/Media/MovieUploadRequest.php
@@ -4,12 +4,12 @@ namespace App\Http\Requests\Media;
 
 class MovieUploadRequest extends AbstractMediaUploadRequest
 {
-    private $data;
+    private $dataArr;
     private $option;
 
     public function __construct(array $data = [], array $option = [])
     {
-        $this->data = $data;
+        $this->dataArr = $data;
         $this->option = $option;
     }
 
@@ -25,16 +25,23 @@ class MovieUploadRequest extends AbstractMediaUploadRequest
 
     protected function isArray(): bool
     {
-        return array_key_exists('File', $this->data) && array_key_exists('movie', $this->data['File']) ? is_array($this->data['File']['movie']) : true;
+        if (array_key_exists('File', $this->dataArr) && array_key_exists('movie', $this->dataArr['File'])) {
+
+            return is_array($this->dataArr['File']['movie']);
+        }
+
+        if (count($this->dataArr) === 0) return false;
+
+        return true;
     }
 
     protected function getRules(): array
     {
         $data = [];
-        if (!array_key_exists('File', $this->data)) return $data;
+        if (!array_key_exists('File', $this->dataArr)) return $data;
 
-        if ($this->isArray() && array_key_exists('movie', $this->data['File'])) {
-            foreach ($this->data['File']['movie'] as $key => $value) {
+        if ($this->isArray() && array_key_exists('movie', $this->dataArr['File'])) {
+            foreach ($this->dataArr['File']['movie'] as $key => $value) {
                 $param = ['nullable', 'max:80000', 'mimes:mp4,qt,x-ms-wmv,mpeg,x-msvideo'];
                 if (count($this->option) > 0) {
                     $param = array_merge($param, $this->option);
