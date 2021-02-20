385c5088a7781e9833aeda82e20eb81565a23ad7
ugnic <cidr@realidea.jp>
2021-02-20 20:00:34 +0900

    saver

--- app/Models/AdItem.php
+++ app/Models/AdItem.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Models\Company;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
--- app/Models/Notice.php
+++ app/Models/Notice.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notice extends Model
 {
-    //
+    protected $table = 'notice';
+
+    protected $fillable = [
+        'subject',
+        'content',
+        'target',
+        'is_delivered',
+    ];
+
+    protected $dates = [
+        'delivered_at',
+        'created_at',
+        'updated_at',
+    ];
 }
