a67acfeb46e8c1c3a945c080e6a6e2d562d2dfc3
ugnic <cidr@realidea.jp>
2021-02-22 01:28:26 +0900

    配信フラグ

--- app/Services/NoticeService.php
+++ app/Services/NoticeService.php
@@ -16,13 +16,13 @@ class NoticeService
 
     public function getNoticeForCompany()
     {
-        $notices = Notice::where("target", "全体")->orWhere("target", "企業")->get();
+        $notices = Notice::where("is_delivered", true)->where("target", "全体")->orWhere("target", "企業")->get();
         return $notices;
     }
 
     public function getNoticeForUser()
     {
-        $notices = Notice::where("target", "全体")->orWhere("target", "応募者")->get();
+        $notices = Notice::where("is_delivered", true)->where("target", "全体")->orWhere("target", "応募者")->get();
         return $notices;
     }
 }
